e40bcc36bebdf23f4d7b3cd9e9f1906f17d69674
exuszee <exuszee@gmail.com>
2023-04-19 09:50:00 +0300

    user

--- models/user.py
+++ models/user.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 """ module for USer class """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship, backref
+from os import getenv
+from sqlalchemy import Column, String
 
 
 class User(BaseModel):
     """ a class User that inherits from BaseModel """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes User"""
+        super().__init__(*args, **kwargs)
