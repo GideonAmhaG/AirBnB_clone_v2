4ec53530df22de609a35ce11eff95c98a5fc30fc
GideonAmhaG <gideonamha@gmail.com>
2023-04-19 13:54:35 +0300

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,21 @@
-#!/usr/bin/python3
-""" module for Amenity class """
-from models.base_model import BaseModel
+#!/usr/bin/python
+"""Module for Amenity class"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    """" class that inherits from BaseModel """
-    name = ""
+class Amenity(BaseModel, Base):
+    """Amenity class"""
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
