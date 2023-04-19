384714a95cbdf25034bb1fa274276c92cc313d6d
GideonAmhaG <gideonamha@gmail.com>
2023-04-19 13:50:31 +0300

    review

--- models/review.py
+++ models/review.py
@@ -1,10 +1,24 @@
-#!/usr/bin/python3
-""" module for Review class """
-from models.base_model import BaseModel
+#!/usr/bin/python
+"""Module for Place class"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel):
-    """ class that inherits form BaseModel """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """Review class"""
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        __tablename__ = 'reviews'
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Review"""
+        super().__init__(*args, **kwargs)
