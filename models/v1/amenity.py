698a2058b53a7e368cc57a8bb9f18d2811df0c6b
GideonAmhaG <gideonamha@gmail.com>
2023-05-03 14:11:16 +0300

    clean up

--- models/v1/amenity.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" module for Amenity class """
-from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """" class that inherits from BaseModel """
-    name = ""
--- models/v1/state.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" module for state class """
-from models.base_model import BaseModel
-
-
-class State(BaseModel):
-    """ class that inherits from BaseModel """
-    name = ""
