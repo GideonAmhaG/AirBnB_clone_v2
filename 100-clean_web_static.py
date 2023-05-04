0a3573dd230dc526a3384843f4f02ff397f4bcf4
GideonAmhaG <gideonamha@gmail.com>
2023-05-04 14:24:45 +0300

    test

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,13 +8,20 @@ env.hosts = ["54.175.254.46", "18.210.20.195"]
 
 def do_clean(number=0):
     """Delete out-of-date archives"""
-    number = 1 if int(number) == 0 else int(number)
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
+    if int(number) == 0:
+        number = 1
+    else:
+        number = int(number)
+    archives = os.listdir("versions")
+    archives_sorted = sorted(archives)
+    archives_to_remove = archives_sorted[:-number]
     with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+        for archive in archives_to_remove:
+            local(f"rm {archive}")
+    archives = run("ls -tr /data/web_static/releases").split()
+    archives_filtered = [a for a in archives if "web_static_" in a]
+    archives_filtered_sorted = sorted(archives_filtered)
+    archives_to_remove = archives_filtered_sorted[:-number]
     with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+        for archive in archives_to_remove:
+            run(f"rm -rf {archive}")
